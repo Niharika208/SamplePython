da9022098b13c68fee88be72240f4d5784c47fd2
Niharika208 <anumulaniharika99@gmail.com>
2024-09-16 18:01:03 -0500

    Remaining python files

---
+++ pythonProject/mycode.py
@@ -0,0 +1,17 @@
+class computer:
+    def __init__(self):
+        print("in init")
+
+    def  config(self):
+        print("i5,16gb,1TB")
+
+COM1 = computer()
+COM2 = computer()
+COM1.config()
+COM2.config()
+
+
+
+
+
+
